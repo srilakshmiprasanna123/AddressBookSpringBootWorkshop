6da0c04d38fe28548fb49d963b731c2b5170d319
srilakshmiprasanna@123 <srilakshmiprasanna@123.com>
2022-05-23 14:01:21 +0530

    Added Welcome Message

---
+++ src/main/java/com/example/addressbook/repository/AddressBookRepository.java
@@ -0,0 +1,7 @@
+package com.example.addressbook.repository;
+
+import com.example.addressbook.model.AddressBookEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AddressBookRepository extends JpaRepository<AddressBookEntity, Integer> {
+}

f316a814b6c325baa4547f3adfb6272a837ebdb8
srilakshmiprasanna@123 <srilakshmiprasanna@123.com>
2022-05-24 13:08:53 +0530

    added dto to addressbook

--- src/main/java/com/example/addressbook/model/AddressBookEntity.java
+++ src/main/java/com/example/addressbook/model/AddressBookEntity.java
@@ -1,5 +1,6 @@
 package com.example.addressbook.model;
 
+import com.example.addressbook.dto.AddressDto;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -7,7 +8,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
-
+@Data
 @Entity
 @NoArgsConstructor
 public  class  AddressBookEntity {
@@ -35,67 +36,35 @@ public  class  AddressBookEntity {
         this.phone = addressBookEntity.phone;
     }
 
-    public int getId() {
-        return id;
+    public AddressBookEntity(AddressDto addressDto) {
+
+        this.firstName = addressDto.firstName;
+        this.lastName = addressDto.lastName;
+        this.city = addressDto.city;
+        this.state = addressDto.state;
+        this.zip = addressDto.zip;
+        this.email = addressDto.email;
+        this.phone = addressDto.phone;
+
+
     }
 
-    public void setId(int id) {
+    public AddressBookEntity(Integer id, AddressDto addressDto) {
         this.id = id;
+        this.firstName = addressDto.firstName;
+        this.lastName = addressDto.lastName;
+        this.city = addressDto.city;
+        this.state = addressDto.state;
+        this.zip = addressDto.zip;
+        this.email = addressDto.email;
+        this.phone = addressDto.phone;
+
+
     }
 
-    public String getFirstName() {
-        return firstName;
+    public AddressBookEntity(Integer id) {
+        this.id=id;
     }
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
 
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getState() {
-        return state;
-    }
-
-    public void setState(String state) {
-        this.state = state;
-    }
-
-    public String getZip() {
-        return zip;
-    }
-
-    public void setZip(String zip) {
-        this.zip = zip;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
 }
